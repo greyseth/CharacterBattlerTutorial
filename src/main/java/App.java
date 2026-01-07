3a093bacd3f634ed4fed8afab8930b3b1cee0394
greyseth <anargya2gilland@gmail.com>
2026-01-07 20:56:49 +0700

    pertemuan 2: advanced input handling

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.Scanner;
 
 public class App {
@@ -8,17 +10,46 @@ public class App {
         System.out.println("2 - Battle");
         System.out.println("3 - Exit");
 
-        String userInput = "";
+        int userInput = 0;
+        userInput = inputNumber(3);
+        System.out.println(userInput);
 
-        Scanner scanner = new Scanner(System.in);
-        userInput = scanner.nextLine();
+        if (userInput == 1) {
+            System.out.println("Characters registered: 0");
+            System.out.println("1 - Register New Character");
+            System.out.println("2 - Back");
 
-        if (userInput.equals("1")) {
-            System.out.println("Character List Menu");
-        }else if (userInput.equals("2")) {
+            int userInput2 = inputNumber(2);
+
+            System.out.println("CHaracter list");
+        }else if (userInput == 2) {
             System.out.println("Battle menu");
         }else {
             System.out.println("Goodbye");
         }
+    }
+
+    static int inputNumber(int optionCount) {
+        Scanner scanner = new Scanner(System.in);
+
+        int inputNumber = 0;
+
+        boolean isValid = false;
+        while(!isValid) {
+            try {
+                String rawInput = scanner.nextLine();
+
+                inputNumber = Integer.parseInt(rawInput);
+                if (inputNumber > 0 && inputNumber <= optionCount) {
+                    isValid = true;
+                } else {
+                    System.out.println("Invalid menu input");
+                }
+            } catch (Exception e) {
+                System.out.println("Input must be a number");
+            }
+        }
+
+        return inputNumber;
     }
 }
--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -1,7 +1,13 @@
 public class Testing {
     public static void main(String[] args) {
-        String x = "5";
+        int result = 0;
+        result = addition(4, 5);
 
-        System.out.println(x == "5");
+        System.out.println(result);
+    }
+
+    static int addition(int a, int b) {
+        System.out.println("Adding "+a+" with "+b);
+        return a+b;
     }
 }
